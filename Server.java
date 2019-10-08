3823bf1514c4c4da5f4a73cfcbc90f64f1c838c6
TshegofatsoMathabatha <sjomana16@gmail.com>
2019-10-08 12:23:25 +0200

    Server defined

--- Server.java
+++ Server.java
@@ -8,6 +8,32 @@ public class Server
     private Socket socket = null;
     private ServerSocket server = null;
     private DtaInputStream in = null;
+
+    public Server(int port){
+
+        try{
+            server = new ServerSocket(port);
+            System.out.println("Server Started");
+
+            System.out.println("Waiting for a client to connect...");
+
+            socket = server.accept();
+            System.ou.println("Client connection accepted");
+
+            in = new DtaInputStream(new BufferedInputStream(socket.getInputStream()));
+            String line = "";
+
+            while (!line.equals("Done")) {
+                try{
+                    line = in.readUTF();
+                    system.out.println(line);
+                    socket.close();
+            in.close();
+                }
+                catch(IOException i) {
+                    System.out.println(i);
+                }
+    }
 }
 
     public static void main(String args[])
